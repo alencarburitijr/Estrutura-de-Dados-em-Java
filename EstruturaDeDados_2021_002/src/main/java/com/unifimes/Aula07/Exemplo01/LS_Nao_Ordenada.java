5b336ba4e5cec1d322ef2eb8f0c645bf6dd80009
Alencar Buriti Jr <alencarburitijr@gmail.com>
2021-09-03 19:47:45 -0300

    Aula lista ordenada 1 - Inserir no início 2 - Inserir no fim 3 - Listar

--- EstruturaDeDados_2021_002/src/main/java/com/unifimes/Aula07/Exemplo01/LS_Nao_Ordenada.java
+++ EstruturaDeDados_2021_002/src/main/java/com/unifimes/Aula07/Exemplo01/LS_Nao_Ordenada.java
@@ -12,57 +12,63 @@ import java.util.Scanner;
  * @author alencarburiti
  */
 public class LS_Nao_Ordenada {
-    public static class Lista{
+
+    public static class Lista {
+
         public int num;
         public Lista prox;
-    }  
-    public static void main(String []args){
+    }
+
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        
+
         Lista inicio = null;
         Lista fim = null;
         Lista aux;
+        Lista anterior;
         int operacao;
         int numero;
         int posicao = 1;
-        
-        do{
+        int achou = 0;
+
+        do {
             System.out.println("MENU DE OPÇÕES");
             System.out.println("1 - Inserir no inicio da lista");
             System.out.println("2 - Inserir no fim da lista");
             System.out.println("3 - Listar itens");
-            System.out.println("6 - Sair");                        
-            
+            System.out.println("4 - Remover item da lista");
+            System.out.println("6 - Sair");
+
             //Verifico qual a opção o usuário digitou
             operacao = teclado.nextInt();
-            
+
             // # Inicio operaçao de inserir no início da Lista
-            if (operacao == 1){
+            if (operacao == 1) {
                 System.out.println("Digite o número a ser inserido no início da lista");
                 Lista novo = new Lista();
                 novo.num = teclado.nextInt();
-                if (inicio == null){
+                if (inicio == null) {
                     inicio = novo;
                     fim = novo;
                     fim.prox = null;
-                }else{
+                } else {
                     novo.prox = inicio;
                     inicio = novo;
-                }                
-            }                                      
+                }
+            }
             // # Fim operaçao de inserir no início da Lista
-             
+
             // # Inicio operaçao de inserir no fim da Lista
-            if (operacao == 2){
+            if (operacao == 2) {
                 System.out.println("Digite um número para ser inserido no fim da fila");
                 Lista novo = new Lista();
                 novo.num = teclado.nextInt();
-                
-                if (fim == null){
+
+                if (fim == null) {
                     inicio = novo;
                     fim = novo;
                     fim.prox = null;
-                }else{
+                } else {
                     fim.prox = novo;
                     fim = novo;
                     fim.prox = null;
@@ -70,20 +76,62 @@ public class LS_Nao_Ordenada {
             }
             // # Fim operaçao de inserir no fim da Lista
             // # Inicio lista itens 
-            if (operacao == 3){
+            if (operacao == 3) {
                 System.out.println("Listando itens ... aguarde");
-                if ( inicio == null){
+                if (inicio == null) {
                     System.out.println("Lista vazia");
                 } else {
                     aux = inicio;
-                    while(aux!= null){
-                        System.out.println("A posicao da lista " + posicao + " tem o número " + aux.num);                        
+                    while (aux != null) {
+                        System.out.println("A posicao da lista " + posicao + " tem o número " + aux.num);
                         aux = aux.prox;
-                        posicao ++;
+                        posicao++;
                     }
                 }
             }
             // # Fim lista itens 
-        }while(operacao!=6);               
+
+            // # Inicio remover item da lista
+            if (operacao == 4) {
+                if (inicio == null) {
+                    System.out.println("Lista vazia");
+                } else {
+
+                    aux = inicio;
+                    while (aux != null) {
+                        System.out.println("A posicao da lista " + posicao + " tem o número " + aux.num);
+                        aux = aux.prox;
+                        posicao++;
+                    }
+
+                    System.out.println("Digite qual o número da lista deseja excluir");
+                    numero = teclado.nextInt();
+                    aux = inicio;
+                    anterior = null;
+                    while (aux != null) {
+                        if (aux.num == numero) {
+                            achou++;
+                            if (aux == inicio) {
+                                inicio = aux.prox;
+                                aux = inicio;
+                            } else if (aux == fim) {
+                                anterior.prox = null;
+                                fim = anterior;
+                                aux = null;
+                            } else {
+                                // Anterior
+                                anterior.prox = aux.prox;
+                                aux = null;
+                            }
+                        } else {
+                            anterior = aux;
+                            aux = aux.prox;
+                        }
+                    }
+
+                }
+            }
+            // # Fim remover item da lista
+        } while (operacao != 6);
     }
 }
